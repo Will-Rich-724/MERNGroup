ec80d7ccf8fb7a81d329d0dcd7acd8b71b513283
Will-Rich-724 <williamtrich5@gmail.com>
2021-02-27 13:18:13 -0600

    Added some basic CRUD to controller and routes

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,8 +1,15 @@
 const userController = require("../controllers/user.controller");
 const { authenticate } = require("../configs/jwt.config");
 
+//add authentication to routes that will need to be protected
+
 module.exports = app => {
     app.post("/api/register", userController.register);
     app.post("/api/login", userController.login);
     app.post("/api/logout", userController.logout);
+
+    app.get("/api/users", userController.getAllUsers);
+    app.get("/api/user/:id", userController.getOneUser);
+    app.put("/api/user/:id", userController.updateUser);
+    app.delete("/api/user/:id", userController.deleteUser)
 }
