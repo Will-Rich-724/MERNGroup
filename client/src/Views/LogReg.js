366a4a1633c3be78647dad31461aa72530a7fd2c
Christimwatson <christimwatson@gmail.com>
2021-03-03 14:08:17 -0500

    Updated site for functionality and mobility

--- client/src/Views/LogReg.js
+++ client/src/Views/LogReg.js
@@ -6,9 +6,12 @@ import SignUp from '../Components/SignUp';
 
 const LogReg = (props) => {
     return(
-        <div>
-            <SignIn />
-            <SignUp />
+        <div className='container' style={{marginTop: "50px"}}>
+            <div className='row'>
+                <SignIn className='col-4'  />
+                <p className='col-4'></p>            
+                <SignUp className='col-4' />
+            </div>
         </div>
     )
 } 
