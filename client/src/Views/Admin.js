dbe30b062c5b0ae0677b677629c0b46d0abed47d
Mark Booth <markbooth22@gmail.com>
2021-03-06 16:15:23 -0700

    fix the useeffect functions, fix the schedule and nd appointments page

--- client/src/Views/Admin.js
+++ client/src/Views/Admin.js
@@ -1,60 +1,95 @@
-import React, { useState, useEffect } from 'react';
-import { Router, Link, navigate } from '@reach/router';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Router, Link, navigate } from "@reach/router";
+import axios from "axios";
 
 const Admin = (props) => {
-    const [eventName, setEventName] = useState("American Red Cross Blood Donation Center - Dearborn")
-    const [date, setDate] = useState("2021-03-14");
+    const [eventName, setEventName] = useState(
+        "American Red Cross Blood Donation Center - Dearborn"
+    );
+    const [date, setDate] = useState("2021-02-14");
     const [time, setTime] = useState("10:00 AM");
     const [userId, setUserId] = useState(null);
 
     //axios create appointment
     const addAppointment = (e) => {
-        e.preventDefault()
-        axios.post('http://localhost:8000/api/appointments', {
-            userId,
-            eventName,
-            date,
-            time
-        })
-        .then(res => console.log(res))
-        .catch(err => console.log(err))
-    }
+        e.preventDefault();
+        axios
+            .post("http://localhost:8000/api/appointments", {
+                userId,
+                eventName,
+                date,
+                time,
+            })
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
+    };
 
     return (
         <div>
             <form onSubmit={addAppointment}>
                 <label>Select A Location to Add An Appointment</label>
-                <br/>
-                <select id="location" name="location" input type="text" onChange={(e) => setEventName(e.target.value)}>
-                    <option value="American Red Cross Blood Donation Center - Dearborn">American Red Cross Blood Donation Center - Dearborn</option>
-                    <option value="Bonfils Blood Center - Littleton">Bonfils Blood Center - Littleton</option>
-                    <option value="Children's Hospital Colorado Blood Donor Center - Aurora">Children's Hospital Colorado Blood Donor Center - Aurora</option>
-                    <option value="Riverview Red Cross Blood, Platelet and Plasma Donation Center - Riverview">Riverview Red Cross Blood, Platelet and Plasma Donation Center - Riverview</option>
+                <br />
+                <select
+                    id="location"
+                    name="location"
+                    input
+                    type="text"
+                    onChange={(e) => setEventName(e.target.value)}
+                >
+                    <option value="American Red Cross Blood Donation Center - Dearborn">
+                        American Red Cross Blood Donation Center - Dearborn
+                    </option>
+                    <option value="Bonfils Blood Center - Littleton">
+                        Bonfils Blood Center - Littleton
+                    </option>
+                    <option value="Children's Hospital Colorado Blood Donor Center - Aurora">
+                        Children's Hospital Colorado Blood Donor Center - Aurora
+                    </option>
+                    <option value="Riverview Red Cross Blood, Platelet and Plasma Donation Center - Riverview">
+                        Riverview Red Cross Blood, Platelet and Plasma Donation
+                        Center - Riverview
+                    </option>
                 </select>
-                <br/>
+                <br />
                 <label>Add An Available Date</label>
-                <br/>
-                <select id="location" name="location" input type="text" onChange={(e) => setDate(e.target.value)}>
-                    <option value="2021-03-14">Saturday, March 14th 2021</option>
+                <br />
+                <select
+                    id="location"
+                    name="location"
+                    input
+                    type="text"
+                    onChange={(e) => setDate(e.target.value)}
+                >
+                    <option value="2021-02-14">
+                        Sunday, February 14th 2021
+                    </option>
+                    <option value="2021-03-14">
+                        Saturday, March 14th 2021
+                    </option>
                     <option value="2021-03-15">Sunday, March 15th 2021</option>
                     <option value="2021-03-16">Monday, March 16th 2021</option>
                     <option value="2021-03-17">Tuesday, March 17th 2021</option>
                 </select>
-                <br/>
+                <br />
                 <label>Add an Available Time</label>
-                <br/>
-                <select id="location" name="location" input type="text" onChange={(e) => setTime(e.target.value)}>
+                <br />
+                <select
+                    id="location"
+                    name="location"
+                    input
+                    type="text"
+                    onChange={(e) => setTime(e.target.value)}
+                >
                     <option value="10:00 AM">10:00 AM</option>
                     <option value="11:00 AM">11:00 AM</option>
                     <option value="12:00 PM">12:00 PM</option>
                     <option value="1:00 PM">1:00 PM</option>
                 </select>
-                <br/>
+                <br />
                 <button type="submit">Add Available Appointment</button>
             </form>
         </div>
-    )
-}
+    );
+};
 
 export default Admin;
--- client/src/Views/Appointments.js
+++ client/src/Views/Appointments.js
@@ -1,54 +1,102 @@
-import React, { useState, useEffect } from 'react';
-import { Router, Link, navigate } from '@reach/router';
-import axios from 'axios';
-import LogOutButton from '../Components/LogOutButton';
+import React, { useState, useEffect } from "react";
+import { Router, Link, navigate } from "@reach/router";
+import axios from "axios";
+import LogOutButton from "../Components/LogOutButton";
 
 const Appointments = (props) => {
-    const [ userAccount, setUserAccount ] = useState("");
-    const [ firstName, setFirstName ] = useState("");
-    const [ lastName, setLastName ] = useState("");
-    const [ allAppointments, setAllAppointments ] = useState([]);
+    const [userAccount, setUserAccount] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [allAppointments, setAllAppointments] = useState([]);
 
     // var today = new.date(today)
     //axios get logged in user
     useEffect(() => {
-        axios.get("http://localhost:8000/api/users/user/loggedin", {
-            withCredentials: true
-        })
-        .then(res => setUserAccount(res.data._id))
-    },[]);
+        axios
+            .get("http://localhost:8000/api/users/user/loggedin", {
+                withCredentials: true,
+            })
+            .then((res) => {
+                setUserAccount(res.data._id);
+                axios
+                    .get(`http://localhost:8000/api/user/${res.data._id}`, {
+                        withCredentials: true,
+                    })
+                    .then((res) => {
+                        setFirstName(res.data.firstName);
+                        setLastName(res.data.lastName);
+                    })
+                    .catch((err) => console.log(err));
+                axios
+                    .get(
+                        "http://localhost:8000/api/appointment/usersappointments",
+                        {
+                            withCredentials: true,
+                        }
+                    )
+                    .then((res) => {
+                        console.log(res.data);
+                        setAllAppointments(res.data);
+                    })
+                    .catch((err) => console.log(err));
+            });
+    }, []);
 
-    //axios get logged in users info
-    useEffect(() => {
-        axios.get(`http://localhost:8000/api/user/${userAccount}`, {
-            withCredentials: true
-        })
-        .then(res => {
-            setFirstName(res.data.firstName);
-            setLastName(res.data.lastName)
-        })
-        .catch(err => console.log(err))
-    }, [])
+    const formatDate = (date, time) => {
+        let [hourMin, amOrPm] = time.split(" ");
+        let [hour, min] = hourMin.split(":");
+        hour = parseInt(hour);
+        if (amOrPm === "PM" && hour !== 12) {
+            hour += 12;
+        }
+        if (amOrPm === "AM" && hour === 12) {
+            hour = 0;
+        }
+        let [year, monthIndex, dayNum] = date.split("-");
+        monthIndex = parseInt(monthIndex) - 1;
+        return new Date(year, monthIndex, dayNum, hour, min);
+    };
 
-    useEffect(() => {
-        axios.get("http:localhost:8000/api/appoinment/usersappointments", {
-            withCredentials: true
-        })
-        .then(res => setAllAppointments(res.data))
-        .catch(err => console.log(err))
-    }, [])
+    const prevApt = allAppointments.filter(
+        (appointment) =>
+            formatDate(appointment.date, appointment.time) < new Date()
+    );
+
+    const upcomingApt = allAppointments.filter(
+        (appointment) =>
+            formatDate(appointment.date, appointment.time) > new Date()
+    );
 
     return (
-        <div className='container' style={{padding: "20px"}}>
+        <div className="container" style={{ padding: "20px" }}>
             <h3> Hi {firstName} here is a list of your appointments:</h3>
             <h1>Upcoming Appointments</h1>
-            {/* Nested in an if (if < var today)
-            Map  */}
-            <h1>Past Appointments</h1>
-            {/* Map to table */}
-        </div>
-    )
-}
+            {upcomingApt.map((appointment, idx) => {
+                const {eventName, date, time} = appointment
 
+                return(
+                    <div key={idx}>
+                        <h2>{eventName}</h2>
+                        <p>{date} {time}</p>
+                    </div>
+                )
+            })}
+
+            <h1>Past Appointments</h1>
+
+            {prevApt.map((appointment, idx) => {
+                const {eventName, date, time} = appointment
+
+                return(
+                    <div key={idx}>
+                        <h2>{eventName}</h2>
+                        <p>{date} {time}</p>
+                    </div>
+                )
+            })}
+
+        </div>
+    );
+};
 
 export default Appointments;
